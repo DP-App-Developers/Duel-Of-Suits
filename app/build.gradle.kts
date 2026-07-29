63df5069354cc2365a382e91b22fbf5b583c0309
Dennis Hong <dehong@ebay.com>
2026-07-28 17:35:03 -0700

    targetSdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.dehong.duelofSuits"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.dehong.duelofSuits"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
